02c9d536dda149a368cb41438cc6ad2873f2192f
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-07-11 13:23:56 +0200

    Update personalizaremulos.sh
    fix

--- scriptmodules/supplementary/extrasemulosmenu/personalizaremulos.sh
+++ scriptmodules/supplementary/extrasemulosmenu/personalizaremulos.sh
@@ -13,14 +13,14 @@ rp_module_id="personalizaremulos"
 rp_module_desc="Personalizar el sistema EmulOS"
 rp_module_section=""
 
-#infobox="${infobox}___________________________________________________\n\n"
-#infobox="${infobox}\nHerramienta para personalizar EmulOS\n"
-#infobox="${infobox}\nMasOS Team no se hace responsable de un mal uso de estos scripts\n\n"
-#infobox="${infobox}\n"
+infobox="${infobox}___________________________________________________\n\n"
+infobox="${infobox}\nHerramienta para personalizar EmulOS\n"
+infobox="${infobox}\nMasOS Team no se hace responsable de un mal uso de estos scripts\n\n"
+infobox="${infobox}\n"
 
-#dialog --backtitle "http://masos.dx.am		MasOS Team" \
-#--title "Personalizacion de EmulOS (by Moriggy)" \
-#--msgbox "${infobox}" 15 55
+dialog --backtitle "http://masos.dx.am		MasOS Team" \
+--title "Personalizacion de EmulOS (by Moriggy)" \
+--msgbox "${infobox}" 15 55
 
 function main_menu() {
 
@@ -29,17 +29,15 @@ function main_menu() {
         choice=$(dialog --backtitle "MasOS Team		Personalización de EmulOS (script hecho por Moriggy)" --title " MENU PRINCIPAL " \
             --ok-label OK --cancel-label Exit \
             --menu "Que acción te gustaría personalizar?" 25 75 20 \
-			1 "Instalar Bezels" \
-			2 "Vídeos de Carga de Roms" \
-			3 "Overclock (SÓLO) Raspberry pi" \
-			# 4 "Arranque silencioso" \
-			2>&1 > /dev/tty)
+						1 "Instalar Bezels" \
+						2 "Vídeos de Carga de Roms" \
+						3 "Overclock (SÓLO) Raspberry pi" \
+						2>&1 > /dev/tty)
 
         case "$choice" in
 			1) bezels  ;;
 			2) launching_videos  ;;
 			3) overclock  ;;
-			4) silencio  ;;
 			*)  break ;;
         esac
     done
@@ -66,14 +64,6 @@ function launching_videos() {
 function overclock() {
 
 	source $scriptdir/scriptmodules/extras/scripts/overclock.sh
-
-}
-
-# Funcion para arranque silencioso	#
-
-function silencio() {
-
-	source $scriptdir/scriptmodules/extras/scripts/sym.sh
 
 }
 
