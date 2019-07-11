2f53484403a71c127b1bf40c5bfdc89c9173060d
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-07-11 10:35:10 +0200

    prueba rp
    prueba de enlace simbolico a personalizar emulos en instalacion basica

--- scriptmodules/admin/setup.sh
+++ scriptmodules/admin/setup.sh
@@ -594,8 +594,8 @@ function gui_setup() {
                     #### gancho nuevo copia de scripts nuestros
               			if [[ -f "/home/pi/EmulOS/emulosmenu/raspiconfig.rp" ]]; then
                 			cd
-                			sudo cp /home/pi/EmulOS-Setup/scriptmodules/extras/gamelist.xml /opt/emulos/configs/all/emulationstation/gamelists/emulos/
-                			sudo cp -R /home/pi/EmulOS-Setup/scriptmodules/supplementary/emulosmenu/* /home/pi/EmulOS/emulosmenu/
+                			#sudo cp /home/pi/EmulOS-Setup/scriptmodules/extras/gamelist.xml /opt/emulos/configs/all/emulationstation/gamelists/emulos/
+                			#sudo cp -R /home/pi/EmulOS-Setup/scriptmodules/supplementary/emulosmenu/* /home/pi/EmulOS/emulosmenu/
                 			sudo cp -R /home/pi/EmulOS-Setup/scriptmodules/extras/scripts /home/pi/EmulOS/
                			  sudo chmod -R +x /home/pi/EmulOS
                 			sudo chmod -R +x /opt/
---
+++ scriptmodules/supplementary/personalizaremulos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+rp_module_id="personalizaremulos"
+rp_module_desc="Podrás añadir bezels, videoloading screen a tus sistemas y hacer Overclock a tu RPI"
+rp_module_section="config"
+
+function gui_personalizaremulos (){
+  source $scriptdir/scriptmodules/supplementary/emulosmenu/personalizaremulos.sh
+}
