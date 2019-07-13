6292f295bde6e321f132b7bd1202e5b5053dd3cd
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-07-13 01:39:36 +0100

    Flycast big update ,plataforms adds
    add autoconfig and systems for atomiswave and naomi.

--- scriptmodules/libretrocores/lr-flycast.sh
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The RetroPie Project | EDITADO Y MEJORADO POR EL EQUIPO MASOS-TEAM | EMULOS |
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -10,8 +10,8 @@
 #
 
 rp_module_id="lr-flycast"
-rp_module_desc="Dreamcast emulator - Reicast port for libretro"
-rp_module_help="Previously named lr-reicast then lr-beetle-dc\n\nDreamcast ROM Extensions: .cdi .gdi .chd, Naomi/Atomiswave ROM Extension: .zip\n\nCopy your Dreamcast/Naomi roms to $romdir/dreamcast\n\nCopy the required Dreamcast BIOS files dc_boot.bin and dc_flash.bin to $biosdir/dc\n\nCopy the required Naomi/Atomiswave BIOS files naomi.zip and awbios.zip to $biosdir/dc"
+rp_module_desc="Dreamcast - Naomi - Atomiswave emulator - Flycast port for libretro"
+rp_module_help="Previously named lr-reicast then lr-flycast\n\nDreamcast ROM Extensions: .cdi .gdi .chd, Naomi/Atomiswave ROM Extension: .lst .bin .zip\n\nCopy your Dreamcast roms to $romdir/dreamcast\n\nCopy your Atomiswave roms to $romdir/atomiswave\n\nCopy your Naomi roms to $romdir/naomi\n\nCopy the required Dreamcast BIOS files dc_boot.bin and dc_flash.bin to $biosdir/dc\n\nCopy the required Naomi/Atomiswave BIOS files naomi.zip and awbios.zip to $biosdir/dc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/flycast/master/LICENSE"
 rp_module_section="exp"
 rp_module_flags="!mali !armv6"
@@ -47,7 +47,11 @@ function install_lr-flycast() {
 
 function configure_lr-flycast() {
     mkRomDir "dreamcast"
+	mkRomDir "naomi"
+	mkRomDir "atomiswave"
     ensureSystemretroconfig "dreamcast"
+	ensureSystemretroconfig "naomi"
+	ensureSystemretroconfig "atomiswave"
 
     mkUserDir "$biosdir/dc"
 
@@ -56,6 +60,10 @@ function configure_lr-flycast() {
     iniSet "video_shared_context" "true"
 
     # segfaults on the rpi without redirecting stdin from </dev/null
-    addEmulator 0 "$md_id" "dreamcast" "$md_inst/flycast_libretro.so </dev/null"
+    addEmulator 1 "$md_id" "dreamcast" "$md_inst/flycast_libretro.so </dev/null"
     addSystem "dreamcast"
+	addEmulator 1 "$md_id" "naomi" "$md_inst/flycast_libretro.so </dev/null"
+	addSystem "naomi"
+	addEmulator 1 "$md_id" "atomiswave" "$md_inst/flycast_libretro.so </dev/null"
+	addSystem "atomiswave"
 }
