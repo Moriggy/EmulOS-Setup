1ad6ddfc78a93930729ac5536a47df374bb43ed0
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-07-15 01:16:33 +0100

    Update autostart.sh
    EmulOS :alien:

--- scriptmodules/supplementary/autostart.sh
+++ scriptmodules/supplementary/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The RetroPie Project and EmulOS
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
