2398434dc91eb7e26f7f78bbe8a652051b8c3c27
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-07-16 21:53:28 +0200

    neo geo cd
    fix emulador

--- scriptmodules/libretrocores/lr-fbneo.sh
+++ scriptmodules/libretrocores/lr-fbneo.sh
@@ -74,7 +74,7 @@ function configure_lr-fbneo() {
     addEmulator 0 "$md_id-neocd" "arcade" "$md_inst/fbneo_libretro.so --subsystem neocd"
     addEmulator $def "$md_id" "neogeo" "$md_inst/fbneo_libretro.so"
     addEmulator 0 "$md_id-neocd" "neogeo" "$md_inst/fbneo_libretro.so --subsystem neocd"
-    addEmulator $def "$md_id" "neogeocd" "$md_inst/fbneo_libretro.so"
+    addEmulator $def "$md_id-neocd" "neogeocd" "$md_inst/fbneo_libretro.so"
     addEmulator $def "$md_id" "fba" "$md_inst/fbneo_libretro.so"
     addEmulator 0 "$md_id-neocd" "fba" "$md_inst/fbneo_libretro.so --subsystem neocd"
 
