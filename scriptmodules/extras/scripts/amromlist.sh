bfbbc1c7f27c3553628bc331a93e68c0fec5f2d8
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-08-04 18:19:02 +0200

    Update amromlist.sh
    update

--- scriptmodules/extras/scripts/amromlist.sh
+++ scriptmodules/extras/scripts/amromlist.sh
@@ -12,6 +12,40 @@ clear
 rm /tmp/showdisplay.txt > /dev/null 2>&1
 rm /tmp/hidedisplay.txt > /dev/null 2>&1
 
+function show_display_nested() {
+systemname=$1
+if [[ "${systemname}" == "MAME (Advance)" ]]; then
+systemname="MAME \(Advance\)"
+fi
+if [[ "${systemname}" == "MAME (Libretro)" ]]; then
+systemname="MAME \(Libretro\)"
+fi
+if [[ "${systemname}" == "MAME (Mame4all)" ]]; then
+systemname="MAME \(Mame4all\)"
+fi
+perl -pi -w -e "s/#${systemname};/${systemname};/g;" /home/pi/.attract/romlists/Arcades.txt
+perl -pi -w -e "s/#${systemname};/${systemname};/g;" /home/pi/.attract/romlists/Computers.txt
+perl -pi -w -e "s/#${systemname};/${systemname};/g;" /home/pi/.attract/romlists/Consoles.txt
+perl -pi -w -e "s/#${systemname};/${systemname};/g;" /home/pi/.attract/romlists/Handhelds.txt
+}
+
+function hide_display_nested() {
+systemname=$1
+if [[ "${systemname}" == "MAME (Advance)" ]]; then
+systemname="MAME \(Advance\)"
+fi
+if [[ "${systemname}" == "MAME (Libretro)" ]]; then
+systemname="MAME \(Libretro\)"
+fi
+if [[ "${systemname}" == "MAME (Mame4all)" ]]; then
+systemname="MAME \(Mame4all\)"
+fi
+perl -pi -w -e "s/^${systemname};/#${systemname};/g;" /home/pi/.attract/romlists/Arcades.txt
+perl -pi -w -e "s/^${systemname};/#${systemname};/g;" /home/pi/.attract/romlists/Computers.txt
+perl -pi -w -e "s/^${systemname};/#${systemname};/g;" /home/pi/.attract/romlists/Consoles.txt
+perl -pi -w -e "s/^${systemname};/#${systemname};/g;" /home/pi/.attract/romlists/Handhelds.txt
+}
+
 function show_display_traditional() {
 currentdisplay="${1}"
 ischoice="false"
@@ -112,6 +146,11 @@ done < /home/pi/.attract/amboot/amromlist.info
 # Actualice los archivos romlist anidados #
 ###########################################
 
+cp /home/pi/.attract/romlists/Arcades.txt /home/pi/.attract/romlists/Arcades.txt.bkp
+cp /home/pi/.attract/romlists/Computers.txt /home/pi/.attract/romlists/Computers.txt.bkp
+cp /home/pi/.attract/romlists/Consoles.txt /home/pi/.attract/romlists/Consoles.txt.bkp
+cp /home/pi/.attract/romlists/Handhelds.txt /home/pi/.attract/romlists/Handhelds.txt.bkp
+
 while read systemname
 do
 show_display_nested "${systemname}"
@@ -129,6 +168,34 @@ done < /tmp/hidedisplay.txt
 cp /home/pi/.attract/attract.cfg /home/pi/.attract/attract.cfg.bkp
 cp /home/pi/.attract/attract.cfg /tmp/attract_tmp.cfg
 
+isarcade=`cat /home/pi/.attract/romlists/Arcades.txt |grep -v "#" |grep -v "Classic" |grep -v "Capcom" |wc -l`
+iscomputer=`cat /home/pi/.attract/romlists/Computers.txt |grep -v "#" |wc -l`
+isconsole=`cat /home/pi/.attract/romlists/Consoles.txt |grep -v "#" |wc -l`
+ishandheld=`cat /home/pi/.attract/romlists/Handhelds.txt |grep -v "#" |wc -l`
+
+if [[ $isarcade > "0" ]]; then
+  show_display_traditional "Arcades"
+else
+  hide_display_traditional "Arcades"
+fi
+mv /tmp/temp.cfg /tmp/attract_tmp.cfg
+if [[ $iscomputer > "0" ]]; then
+  show_display_traditional "Computers"
+else
+  hide_display_traditional "Computers"
+fi
+mv /tmp/temp.cfg /tmp/attract_tmp.cfg
+if [[ $isconsole > "0" ]]; then
+  show_display_traditional "Consoles"
+else
+  hide_display_traditional "Consoles"
+fi
+mv /tmp/temp.cfg /tmp/attract_tmp.cfg
+if [[ $ishandheld > "0" ]]; then
+  show_display_traditional "Handhelds"
+else
+  hide_display_traditional "Handhelds"
+fi
 mv /tmp/temp.cfg /tmp/attract_tmp.cfg
 
 cp /tmp/attract_tmp.cfg /home/pi/.attract/attract.cfg
