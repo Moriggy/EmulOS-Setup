dff9cda2bde7538cd8513e18e5d351d87d1425f9
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-08-09 10:38:11 +0200

    update scritp AM
    creacion script para lista de favoritos

--- scriptmodules/extras/scripts/amromlist.sh
+++ scriptmodules/extras/scripts/amromlist.sh
@@ -8,69 +8,130 @@
 
 IFS=";"
 #clear
-attract_dir="/opt/emulos/configs/all/attractmode"
-#################################################
-# Si hay roms en el sistema, muestralo en la wheel
+user="$(cat /etc/passwd | grep '1000' | cut -d: -f1)"
+ruta="/opt/emulos/configs/all/attractmode"
 
-config="$attract_dir/attract.cfg"
-tab=$'\t'
-if [[ -f "$config" ]] && ! grep -q "display$tab$fullname" "$config"; then
-    cp "$config" "$config.bak"
-    if [[ "$fullname" == "EmulOS" ]]; then
-    cat >>"$config" <<_EOF_
-display${tab}$fullname
-${tab}layout               robo
-${tab}romlist              $fullname
-_EOF_
-  else
-    if [ "$(ls $path)" ]; then
-    cat >>"$config" <<_EOF_
-display${tab}$fullname
-${tab}layout               robospin_v4
-${tab}romlist              $fullname
-${tab}in_cycle             yes
-${tab}in_menu              yes
-_EOF_
-    else
-    cat >>"$config" <<_EOF_
-display${tab}$fullname
-${tab}layout               robospin_v4
-${tab}romlist              $fullname
-${tab}in_cycle             no
-${tab}in_menu              no
-_EOF_
-    fi
-  fi
-    chown $user:$user "$config"
+#########################################################
+# Comprobar que sistemas tienen ROMS
+#
+
+ifexist=0
+while read sname romdir ext
+do
+if [[ -d "/home/$user/EmulOS/roms/${romdir}" ]];then
+ ext=`echo "$ext" | sed "s/ /|/g"`
+ files=`ls /home/$user/EmulOS/roms/${romdir} |egrep ${ext}` 2> /dev/null
+ if [[ -z $files ]];then
+  ifexist=0
+  echo "${sname}" >> /tmp/hidedisplay.txt
+ else
+  ifexist=1
+  echo "${sname}" >> /tmp/showdisplay.txt
+ fi
 fi
+done < $ruta/amboot/amromlist.info
 
-#################################################
-# Si hay roms en el sistema, crea listado
+################################################
+# Actualizar menú principal en attract.cfg
 
-[[ ! -d "$attract_dir" ]] && return 0
+cp $ruta/attract.cfg $ruta/attract.cfg.bkp
+cp $ruta/attract.cfg /tmp/attract_tmp.cfg
 
-system_name="$1"
-system_fullname="$2"
-path="$3"
-name="$4"
-desc="$5"
-image="$6"
-
-configs="$attract_dir/romlists/$system_fullname.txt"
-
-# remove extension
-path="${path/%.*}"
-
-if [[ ! -f "$configs" ]]; then
-    echo "#Name;Title;Emulator;CloneOf;Year;Manufacturer;Category;Players;Rotation;Control;Status;DisplayCount;DisplayType;AltRomname;AltTitle;Extra;Buttons" >"$config"
+function show_display_traditional() {
+currentdisplay="${1}"
+ischoice="false"
+let i=0 # define counting variable
+W=() # define working array
+while read -r line; do # process file by file
+    let i=$i+1
+    W+=($i "$line")
+done < <(cat /tmp/showdisplay.txt)
+while read line
+do
+if [[ $line == "display"*"${currentdisplay}" ]]; then
+  echo $line >> /tmp/temp.cfg
+  ischoice="true"
+elif [[ $line == "display"* && $line != "displays_menu_exit"* ]]; then
+  echo $line >> /tmp/temp.cfg
+  ischoice="false"
+elif [[ $line == *"in_cycle"* && $ischoice = "true" ]]; then
+  echo -e "\tin_cycle             yes" >> /tmp/temp.cfg
+elif [[ $line == *"in_menu"* && $ischoice = "true" ]]; then
+  echo -e "\tin_menu              yes" >> /tmp/temp.cfg
+  ischoice="false"
+elif [[ $line == "rule"* ]]; then
+  echo -e "${line}" >> /tmp/temp.cfg
+elif [[ $line == "sound" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "input_map" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "general" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "saver_config" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "layout_config"* ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "intro_config" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "#"* ]]; then
+  echo $line >> /tmp/temp.cfg
+else
+  echo -e "${line}" >> /tmp/temp.cfg
 fi
+done < /tmp/attract_tmp.cfg
+}
 
-# if the entry already exists, remove it
-if grep -q "^$path;" "$configs"; then
-    sed -i "/^$path/d" "$configs"
+function hide_display_traditional() {
+currentdisplay="${1}"
+ischoice="false"
+let i=0 # define counting variable
+W=() # define working array
+while read -r line; do # process file by file
+    let i=$i+1
+    W+=($i "$line")
+done < <(cat /tmp/hidedisplay.txt)
+
+while read line
+do
+  if [[ $line == "display"*"EmulOS" || $line == "display"*"Favorites" ]]; then
+    echo $line >> /tmp/temp.cfg
+    ischoice="true"
+elif [[ $line == "display"* && $line != "displays_menu_exit"* ]]; then
+  echo $line >> /tmp/temp.cfg
+  ischoice="false"
+elif [[ $line == *"in_cycle"* && $ischoice = "true" ]]; then
+  echo -e "\tin_cycle             yes" >> /tmp/temp.cfg
+elif [[ $line == *"in_menu"* && $ischoice = "true" ]]; then
+  echo -e "\tin_menu              yes" >> /tmp/temp.cfg
+  ischoice="false"
+elif [[ $line == *"in_cycle"* && $ischoice = "false" ]]; then
+  echo -e "\tin_cycle             no" >> /tmp/temp.cfg
+elif [[ $line == *"in_menu"* && $ischoice = "false" ]]; then
+  echo -e "\tin_menu              no" >> /tmp/temp.cfg
+elif [[ $line == "rule"* ]]; then
+  echo -e "${line}" >> /tmp/temp.cfg
+elif [[ $line == "sound" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "input_map" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "general" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "saver_config" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "layout_config"* ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "intro_config" ]]; then
+  echo $line >> /tmp/temp.cfg
+elif [[ $line == "#"* ]]; then
+  echo $line >> /tmp/temp.cfg
+else
+  echo -e "${line}" >> /tmp/temp.cfg
 fi
+done < /tmp/attract_tmp.cfg
+}
 
-echo "$path;$name;$system_fullname;;;;;;;;;;;;;;" >>"$configs"
-chown $user:$user "$configs"
-
+#show_display_traditional
+hide_display_traditional
+#rm /tmp/attract_tmp.cfg
+#cp /tmp/temp.cfg $ruta/attract.cfg
 #clear
--- scriptmodules/supplementary/attractmode.sh
+++ scriptmodules/supplementary/attractmode.sh
@@ -72,6 +72,66 @@ sleep 2
 sudo reboot
 _EOF_
 
+      cat > /home/$user/EmulOS/roms/emulos/Favoritos.sh <<_EOF_cd /home/pi/.attract/romlists
+rm Favorites.tag
+
+clear
+echo
+echo "Este script va a generar una nueva lista de juegos llamada Favorites.txt."
+echo
+echo "Esta nueva lista contendrá todos los juegos que hayas marcado como favoritos de todos los los sistemas distintos."
+echo
+sleep 2
+
+echo
+echo "Renombrando el viejo fichero de Favorites.txt ....."
+sleep 1
+mv Favorites.txt Favorites.txt.backup
+
+echo
+echo "Recopilación de lista de archivos de favoritos etiquetados ....."
+sleep 1
+
+echo
+echo "Generando nuevo fichero de Favorites.txt ....."
+sleep 1
+echo
+
+ls *.tag > tagfiles
+
+while read filename
+do
+echo "Usando ${filename} ....."
+echo
+
+  while read gamename
+  do
+    romlist=`echo ${filename} |cut -f1 -d '.'`
+    echo "     Buscando ${romlist}.txt para ${gamename} ....."
+    cat "${romlist}.txt"|egrep "^${gamename};" >> Favorites.txt
+    cat "${romlist}.txt"|grep "[;]${gamename}[;]" >> Favorites.txt
+  done < "${filename}"
+
+sleep 5
+echo
+done < tagfiles
+
+rm tagfiles
+
+cat Favorites.txt |sort -u > tmp_favorites.txt
+mv tmp_favorites.txt Favorites.txt
+
+echo "Finalizada la creación del nuevo fichero de Favorites.txt ....."
+echo
+echo
+echo "Con esta nueva lista de rom, cree una nueva pantalla y elija Favoritos como la lista de rom."
+echo
+echo "Es posible que debas actualizar periÃ³dicamente la lista de Favoritos a medida que agregues nuevos juegos a favoritos."
+echo
+echo
+sleep 5
+echo
+_EOF_
         chmod +x /home/$user/EmulOS/emulosmenu/Attract-Mode.sh
         echo sudo /home/$user/EmulOS-Setup/emulos_pkgs.sh emulosmenu launch /home/$user/EmulOS/emulosmenu/audiosettings.rp >> "/home/$user/EmulOS/roms/emulos/Audio.sh"
         echo sudo /home/$user/EmulOS-Setup/emulos_pkgs.sh emulosmenu launch /home/$user/EmulOS/emulosmenu/bluetooth.rp >> "/home/$user/EmulOS/roms/emulos/Bluetooth.sh"
