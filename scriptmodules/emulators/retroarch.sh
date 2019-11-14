6376de4aeba581f82f30f05efa8895ae4a61e11c
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-11-14 15:27:15 +0100

    Update retroarch.sh
    vuelta a la version 1.8.0

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -39,7 +39,7 @@ function depends_retroarch() {
 }
 
 function sources_retroarch() {
-    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.8.1
+    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.8.0
     applyPatch "$md_data/01_hotkey_hack.diff"
     applyPatch "$md_data/02_disable_search.diff"
 }
