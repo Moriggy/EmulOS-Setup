48f137c62417b72fc031d1539a0f99915bbce4e7
Moriggy <41973660+Moriggy@users.noreply.github.com>
2019-11-14 18:17:43 +0100

    reicast
    cambios

--- scriptmodules/emulators/reicast/reicast.sh
+++ scriptmodules/emulators/reicast/reicast.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# This file is part of The EmulOS Project
+# This file is part of The RetroPie Project
 #
-# The EmulOS Project is the legal property of its developers, whose names are
+# The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
-# at https://raw.githubusercontent.com/Moriggy/MoriggyEmulOS-Setup/master/LICENSE.md
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
 AUDIO="$1"
 ROM="$2"
+XRES="$3"
+YRES="$4"
 rootdir="/opt/emulos"
 configdir="$rootdir/configs"
+biosdir="$HOME/EmulOS/BIOS/dc"
 
 source "$rootdir/lib/inifuncs.sh"
 
@@ -72,17 +75,18 @@ function mapInput() {
     echo "$params"
 }
 
-if [[ ! -f "$HOME/EmulOS/BIOS/dc/dc_boot.bin" ]]; then
+if [[ ! -f "$biosdir/dc_boot.bin" ]]; then
     dialog --no-cancel --pause "You need to copy the Dreamcast BIOS files (dc_boot.bin and dc_flash.bin) to the folder $biosdir to boot the Dreamcast emulator." 22 76 15
     exit 1
 fi
 
 params=(-config config:homedir=$HOME -config x11:fullscreen=1)
+[[ -n "$XRES" ]] && params+=(-config x11:width=$XRES -config x11:height=$YRES)
 getAutoConf reicast_input && params+=($(mapInput))
 [[ -n "$AUDIO" ]] && params+=(-config audio:backend=$AUDIO -config audio:disable=0)
 [[ -n "$ROM" ]] && params+=(-config config:image="$ROM")
 if [[ "$AUDIO" == "oss" ]]; then
-    aoss "$rootdir/emulators/reicast/bin/reicast" "${params[@]}" >/dev/null
+    aoss "$rootdir/emulators/reicast/bin/reicast" "${params[@]}"
 else
-    "$rootdir/emulators/reicast/bin/reicast" "${params[@]}" >/dev/null
+    "$rootdir/emulators/reicast/bin/reicast" "${params[@]}"
 fi
